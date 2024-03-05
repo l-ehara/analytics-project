ae1a90b48b509bb16cf0edb3df1b72051c97be9c
l-ehara <lucasehara@gmail.com>
2024-03-05 20:37:47 -0300

    First commit, initial development [development]

---
+++ App.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import polars as pl
+import matplotlib.pyplot as plt
+
+df = pl.read_csv("global_inflation_data.csv")
+
+st.write("""# Global inflation since 1980""")
+st.write("""### Made by [me](https://www.linkedin.com/in/lucasehara/)""")
+
+st.write(df)
