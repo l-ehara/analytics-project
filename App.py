14dd41c648c35198204ee173bd900db206290eba
l-ehara <lucasehara@gmail.com>
2024-03-06 18:09:50 -0300

    Minor change to configure git

--- App.py
+++ App.py
@@ -7,4 +7,4 @@ df = pl.read_csv("global_inflation_data.csv")
 st.write("""# Global inflation since 1980""")
 st.write("""### Made by [me](https://www.linkedin.com/in/lucasehara/)""")
 
-st.write(df)
+st.write(df) 
