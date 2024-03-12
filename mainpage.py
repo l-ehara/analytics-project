94d68d0a8dde42f0c198583d5b726cfe8f078ba0
Lucas Ehara <lehara@nelogica.com.br>
2024-03-12 10:54:53 -0300

    Implement about me page and update some files [development]

--- mainpage.py
+++ mainpage.py
@@ -4,20 +4,30 @@ import polars.selectors as cs
 import plotly.express as px
 import duckdb
 
+
+st.set_page_config(
+    page_title="Global Infaltion Analytics Project",
+    page_icon=":bar_chart:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
 df = pl.read_csv("global_inflation_data.csv", try_parse_dates=True)
 
 st.title("Global inflation since 1980")
-st.write("### This project was made for my personal studies and to practice my skills in data analysis and visualization.")
+st.write("##### This project was made for my personal studies and to practice my skills in data analysis and visualization.")
 
 
 df_melted = df.melt(id_vars=["country_name"], variable_name="year", value_name="inflation")
 
 country_list = df_melted["country_name"]
-selected_country = st.selectbox("Select a country", country_list)
+selected_country = st.selectbox("Select a country", country_list.unique().sort())
 
 
 result = duckdb.sql(f"SELECT country_name, year, inflation FROM df_melted WHERE country_name = '{selected_country}' AND inflation IS NOT NULL ").to_df()
 
+px.defaults.width = 1000
+px.defaults.height = 600
 
 fig = px.line(result, x="year", y="inflation", title=f"Inflation in {selected_country}")
 
--- pages/about_me.py
+++ pages/about_me.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.write("""# Lucas Paprotzki Ehara""")
+st.write("""### Check out my [Linkedin profile](https://www.linkedin.com/in/lucasehara/)""")
+
+st.write("""Hello! I'm currently a 6th semester software engineering student and have always had a deep passion for software development. 
+The journey of coding, creating, and watching a simple concept grow into a complex program truly captivates me. 
+I have an insatiable curiosity for learning, constantly seeking to expand my skills and knowledge in various programming languages, 
+software systems, and innovative technologies.
+
+My academic experience has equipped me with a solid foundation in computer science and software engineering principles. 
+Yet, it's not just the technical aspect of software development that draws me in; 
+I am also fascinated by the role it plays in solving real-world problems and enhancing users' lives.
+
+Looking forward, my ambition is to continually evolve in this ever-changing field of software development and data engineering, 
+staying on top of current trends and advancements. My goal is to apply my passion and knowledge to a forward-thinking company, 
+contributing to impactful projects that push boundaries and facilitate societal advancement.""")
+
